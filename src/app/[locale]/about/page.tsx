a4cd881c7dc7030d71ddaa1444c9dfcfa60d392a
Mutter <mutter45@foxmail.com>
2025-09-21 23:43:43 +0800

    feat: add about page

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,11 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+export default async function AboutPage() {
+  return (
+    <>
+      <Header />
+      <div>hi I am Mutter</div>
+      <Footer />
+    </>
+  )
+}
